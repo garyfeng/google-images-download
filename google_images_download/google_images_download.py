053ece1d52206e55964961ea45081078b0465824
Gary Feng <gary.feng@gmail.com>
2022-03-05 18:36:51 -0500

    #11, fixing the asr format

--- google_images_download/google_images_download.py
+++ google_images_download/google_images_download.py
@@ -1154,7 +1154,7 @@ class googleimagesdownload:
                                 if asr < arguments['aspect_ratio_threshold']:
                                     # delete the image
                                     if not arguments["silent_mode"]:
-                                        print("Deleting Image, aspect ratio {} ====> {}".format(asr, this_image["image_filename"]))
+                                        print("Deleting Image, aspect ratio {:.2f} ====> {}".format(asr, this_image["image_filename"]))
                                     imfile = "{}/{}/{}".format(main_directory, dir_name, this_image["image_filename"])
                                     try:
                                         os.remove(imfile)
